044176f82c60e24186ada6c3d71c6159f2d9400c
Yixing Lao <yixing.lao@gmail.com>
2021-02-02 19:27:00 -0800

    add T=Scalar approach

---
+++ init_list/main.cpp
@@ -0,0 +1,35 @@
+#include <initializer_list>
+#include <iostream>
+#include <type_traits>
+
+class Scalar {
+    int i_;
+    float f_;
+    Scalar(int i) { i_ = i; }
+    Scalar(float f) { f_ = f; }
+};
+
+template <typename T = Scalar>
+void constructor(std::initializer_list<T> a) {
+    static_assert(std::is_same<T, Scalar>(), "Type must be Scalar.");
+    std::cout << "constructor 1D" << std::endl;
+}
+
+template <typename T = Scalar>
+void constructor(std::initializer_list<std::initializer_list<T>> a) {
+    static_assert(std::is_same<T, Scalar>(), "Type must be Scalar.");
+    std::cout << "constructor 2D" << std::endl;
+}
+
+template <typename T = Scalar>
+void constructor(
+        std::initializer_list<std::initializer_list<std::initializer_list<T>>>
+                a) {
+    static_assert(std::is_same<T, Scalar>(), "Type must be Scalar.");
+    std::cout << "constructor 3D" << std::endl;
+}
+
+int main() {
+    constructor({});
+    return 0;
+}
